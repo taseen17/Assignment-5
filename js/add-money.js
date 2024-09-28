12187e027db2d92deb0a52a497575d743186700f
taseen17 <muktadirtaseen98@gmail.com>
2024-09-29 03:10:35 +0600

    done i guess

--- js/add-money.js
+++ js/add-money.js
@@ -1,17 +1,27 @@
 document.getElementById("add-money-btn-nk").addEventListener('click', function () {
     const donate1 = getInputFieldValue("donation-form-1")
+    const accountBalance = getInnerText("balance")
 
-    if (isNaN(donate1) === false) {
+    if (isNaN(donate1) === false && donate1<accountBalance) {
         const noakhaliBalance = getInnerText("noakhali-balance")
         const updatedBalance = donate1 + noakhaliBalance
-        document.getElementById("noakhali-balance").innerText = `${updatedBalance} BDT`
-        const accountBalance = getInnerText("balance")
+        document.getElementById("noakhali-balance").innerText = `${updatedBalance} BDT`   
         const updatedAccountBalance = accountBalance - donate1
         document.getElementById("balance").innerText = `${updatedAccountBalance} BDT`
-        // modal//
         
-
-
+        const transactions = document.createElement('div')
+        transactions.classList.add("border")
+        transactions.classList.add("rounded-xl")
+        transactions.classList.add("p-2")
+        transactions.classList.add("mb-2")
+        let now = new Date();
+        let date = now.toLocaleDateString();
+        let time = now.toLocaleTimeString();
+        transactions.innerHTML = `
+        <h4 class="font-semibold"> ${donate1} Taka is donated for flood in Noakhali </h4>
+        <p>Date: ${date} ${time} </p>
+        `
+        document.getElementById("transaction-container").appendChild(transactions)
 
     }
     else {
@@ -22,14 +32,28 @@ document.getElementById("add-money-btn-nk").addEventListener('click', function (
 
 document.getElementById("add-money-btn-feni").addEventListener('click', function () {
     const donate1 = getInputFieldValue("donation-form-2")
+    const feniBalance = getInnerText("feni-balance")
+    const accountBalance = getInnerText("balance")
 
-    if (isNaN(donate1) === false) {
-        const feniBalance = getInnerText("feni-balance")
+    if (isNaN(donate1) === false && donate1<accountBalance) {   
         const updatedBalance = donate1 + feniBalance
-        document.getElementById("feni-balance").innerText = `${updatedBalance} BDT`
-        const accountBalance = getInnerText("balance")
+        document.getElementById("feni-balance").innerText = `${updatedBalance} BDT`    
         const updatedAccountBalance = accountBalance - donate1
         document.getElementById("balance").innerText = `${updatedAccountBalance} BDT`
+
+        const transactions = document.createElement('div')
+        transactions.classList.add("border")
+        transactions.classList.add("rounded-xl")
+        transactions.classList.add("p-2")
+        transactions.classList.add("mb-2")
+        let now = new Date();
+        let date = now.toLocaleDateString();
+        let time = now.toLocaleTimeString();
+        transactions.innerHTML = `
+        <h4 class="font-semibold"> ${donate1} Taka is donated for flood in Feni </h4>
+        <p>Date: ${date} ${time} </p>
+        `
+        document.getElementById("transaction-container").appendChild(transactions)
 
     }
     else {
@@ -40,14 +64,28 @@ document.getElementById("add-money-btn-feni").addEventListener('click', function
 
 document.getElementById("add-money-btn-quota").addEventListener('click', function () {
     const donate1 = getInputFieldValue("donation-form-3")
+    const quotaBalance = getInnerText("quota-balance")
+    const accountBalance = getInnerText("balance")
 
-    if (isNaN(donate1) === false) {
-        const quotaBalance = getInnerText("quota-balance")
+    if (isNaN(donate1) === false && donate1<accountBalance) {   
         const updatedBalance = donate1 + quotaBalance
-        document.getElementById("quota-balance").innerText = `${updatedBalance} BDT`
-        const accountBalance = getInnerText("balance")
+        document.getElementById("quota-balance").innerText = `${updatedBalance} BDT`    
         const updatedAccountBalance = accountBalance - donate1
         document.getElementById("balance").innerText = `${updatedAccountBalance} BDT`
+
+        const transactions = document.createElement('div')
+        transactions.classList.add("border")
+        transactions.classList.add("rounded-xl")
+        transactions.classList.add("p-2")
+        transactions.classList.add("mb-2")
+        let now = new Date();
+        let date = now.toLocaleDateString();
+        let time = now.toLocaleTimeString();
+        transactions.innerHTML = `
+        <h4 class="font-semibold"> ${donate1} Taka is donated for Aid for Quota Reformation Movement </h4>
+        <p>Date: ${date} ${time} </p>
+        `
+        document.getElementById("transaction-container").appendChild(transactions)
 
     }
     else {
@@ -59,6 +97,22 @@ document.getElementById("add-money-btn-quota").addEventListener('click', functio
 document.getElementById("blog-btn").addEventListener('click',function(){
     window.location.href = '/blog.html'
 })
+
+
+document.getElementById("history-btn").addEventListener('click',function(){
+    const bg1 = document.getElementById("history-btn").classList.add("bg-[#B4F461]") 
+    const bg2 = document.getElementById("donation-btn").classList.remove("bg-[#B4F461]")
+    showElement("history-section")
+    
+})
+
+document.getElementById("donation-btn").addEventListener('click',function(){
+    const bg1 = document.getElementById("donation-btn").classList.add("bg-[#B4F461]") 
+    const bg2 = document.getElementById("history-btn").classList.remove("bg-[#B4F461]")
+    showElement("donation-section")
+    
+})
+
 
 
 
--- js/utilities.js
+++ js/utilities.js
@@ -11,3 +11,16 @@ function getInnerText(id){
     return balanceAmount
 
 }
+
+function showElement(id){
+    const history = document.getElementById("donation-section").classList.add("hidden")
+    const donation = document.getElementById("history-section").classList.add("hidden")
+    const show = document.getElementById(id).classList.remove("hidden")  
+}
+
+function updateDateTime() {
+    var now = new Date();
+    var date = now.toLocaleDateString();
+    var time = now.toLocaleTimeString();
+    document.getElementById('datetime').innerHTML = date + ' ' + time;
+}
